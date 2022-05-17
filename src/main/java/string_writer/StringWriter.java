212d1d68a927686d72da3e6d0e0669d85b183d09
RoksolanaMazurak <93178658+RoksolanaMazurak@users.noreply.github.com>
2022-05-17 10:45:35 +0300

    add lab 7 code

---
+++ src/main/java/string_writer/StringWriter.java
@@ -0,0 +1,43 @@
+package string_writer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+public class StringWriter {
+    private int strSize;
+    private String question;
+    public String writeString(StringWriter stringWriter) throws IllegalArgumentException {
+
+        if (getStrSize() < 0) {
+            throw new IllegalArgumentException("String size cannot contain '-'.");
+        }
+
+        System.out.println("Expected number of characters: " + getStrSize());
+
+        String real_len = getQuestion().replaceAll("\\s+", "");
+        int realStrLen = real_len.length();
+        System.out.println("Real number of characters in question: " + realStrLen);
+
+        if (realStrLen <= getStrSize()) {
+            System.out.println(getQuestion() + "?");
+        } else {
+            String temp = new StringBuilder(getQuestion()).toString();
+            List<String> str = Arrays.asList(temp.split(" "));
+            Collections.reverse(str);
+            System.out.println(str);
+        }
+        return getQuestion();
+    }
+}
